2e78e2c43f3bf45db81f3545fcdbd5fdf44af17e
ElusiveFluffy <Freya8822@hotmail.com>
2025-01-13 14:09:34 +1030

    Add project files.

---
+++ Hub 4 Restored/dllmain.cpp
@@ -0,0 +1,35 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include "TygerFrameworkAPI.hpp"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+EXTERN_C void TygerFrameworkPluginRequiredVersion(TygerFrameworkPluginVersion* version) {
+    //Specifiy the version number defined in the API
+    version->Major = TygerFrameworkPluginVersion_Major;
+    version->Minor = TygerFrameworkPluginVersion_Minor;
+    version->Patch = TygerFrameworkPluginVersion_Patch;
+
+    version->CompatibleGames = { 1 };
+}
+
+EXTERN_C bool TygerFrameworkPluginInitialize(TygerFrameworkPluginInitializeParam* param) {
+
+    API::Initialize(param);
+
+    return true;
+}
