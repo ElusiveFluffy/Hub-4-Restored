f75d3cb740d1fa99e331e4ab9ad7e6f6762d7bff
ElusiveFluffy <Freya8822@hotmail.com>
2025-01-15 22:21:50 +1030

    Use the function I added to TygerMemory to write hard coded values

--- Hub 4 Restored/TyMemoryValues.h
+++ Hub 4 Restored/TyMemoryValues.h
@@ -4,29 +4,8 @@
 
 namespace TyMemoryValues {
 
-	void inline WriteHardcodedInt(UINT* address, int value) {
-		DWORD oldProtection;
-		//Change the memory access to ReadWrite to be able to change the hardcoded value (usually its read only)
-		VirtualProtect(address, 4, PAGE_EXECUTE_READWRITE, &oldProtection);
-
-		*address = value;
-
-		//Set it back to the old access protection
-		VirtualProtect(address, 4, oldProtection, &oldProtection);
-	}
-	void inline WriteHardcodedByte(BYTE* address, BYTE value) {
-		DWORD oldProtection;
-		//Change the memory access to ReadWrite to be able to change the hardcoded value (usually its read only)
-		VirtualProtect(address, 1, PAGE_EXECUTE_READWRITE, &oldProtection);
-
-		*address = value;
-
-		//Set it back to the old access protection
-		VirtualProtect(address, 1, oldProtection, &oldProtection);
-	}
-
 	//To disable the game info option in the credits
 	void inline DisableGameInfoID(LevelCode levelID) { 
-		WriteHardcodedByte((BYTE*)(Core::moduleBase + 0xE42E2), (BYTE)levelID);
+		Core::SetReadOnlyValue((BYTE*)(Core::moduleBase + 0xE42E2), &levelID, 1);
 	};
 }
