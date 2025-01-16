9e6187c19faf84ed4c17e59537789e02dc3f57d5
ElusiveFluffy <Freya8822@hotmail.com>
2025-01-16 21:58:44 +1030

    Remove unused functions

--- Hub 4 Restored/PluginCore.h
+++ Hub 4 Restored/PluginCore.h
@@ -4,7 +4,5 @@ namespace PluginCore
 	bool Setup();
 	void OnTyInitialized();
 	void Tick(float deltaSeconds);
-	bool ImGuiWantCaptureMouse();
-	bool WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 };
 
