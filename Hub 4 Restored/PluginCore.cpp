44f46a66844b8fc1cc9e2cd61f2b1511590e5460
ElusiveFluffy <Freya8822@hotmail.com>
2025-01-16 20:43:10 +1030

    Copy minimap transforms over from the old level IDs to the new ones

--- Hub 4 Restored/PluginCore.cpp
+++ Hub 4 Restored/PluginCore.cpp
@@ -6,6 +6,7 @@
 
 //TygerMemory
 #include "core.h"
+#include "minimap.h"
 #include "gamestate.h"
 
 bool PluginCore::Setup()
@@ -18,6 +19,12 @@ bool PluginCore::Setup()
 void PluginCore::OnTyInitialized()
 {
     LevelSelect::SetupMultiColumn();
+    //Setup minimap
+    Minimap::SetDisabledMapLevel((char)LevelCode::E3);
+    Minimap::SetMapTransform(LevelCode::Z3, Minimap::GetMapTransform(LevelCode::E2));
+    Minimap::SetMapTransform(LevelCode::E2, Minimap::GetMapTransform(LevelCode::D2));
+    Minimap::SetMapTransform(LevelCode::E3, Minimap::GetMapTransform(LevelCode::D1));
+    Minimap::SetMapTransform(LevelCode::D1, Minimap::GetMapTransform(LevelCode::B3));
 }
 
 void PluginCore::Tick(float deltaSeconds)
